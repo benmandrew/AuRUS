b95174ce38bc63fa02565d2541638172ff5406d1
benmandrew <benmandrew@gmail.com>
2026-01-14 18:24:17 +0000

    feat: Conditionally use async-profiler
    Produces a flame chart for testing optimisations.

--- sort-solutions.sh
+++ sort-solutions.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-ant compile
+if ! ant compile
+then
+    echo "Build failed"
+    exit 1
+fi
 
-java -Xmx8g -Djava.library.path=/usr/local/lib -cp "bin:lib/commons-math3-3.6.1.jar:lib/rltlconv.jar:lib/JFLAP-7.0_With_Source.jar:lib/owl-18.10-snapshot.jar:lib/ejml/ejml-core-0.34.jar:lib/ejml/ejml-cdense-0.34.jar:lib/ejml/ejml-ddense-0.34.jar:lib/ejml/ejml-fdense-0.34.jar:lib/ejml/ejml-simple-0.34.jar:lib/ejml/ejml-zdense-0.34.jar:lib/ejml/ejml-dsparse-0.34.jar:lib/ejml/ejml-experimental-0.34.jar:lib/ltl2buchi.jar" main.SortSolutions "$@"
+profile=0
+args=()
+for arg in "$@"; do
+    if [ "$arg" = "--profile" ]; then
+        profile=1
+    else
+        args+=("$arg")
+    fi
+done
+
+agent_opts=()
+if [ $profile -eq 1 ]; then
+    agent_opts=("-agentpath:${HOME}/Downloads/async-profiler-4.2.1-linux-x64/lib/libasyncProfiler.so=start,event=cpu,file=profile.html")
+fi
+
+java \
+    -Xmx8g \
+    -Djava.library.path=/usr/local/lib \
+    -cp "bin:lib/commons-math3-3.6.1.jar:lib/rltlconv.jar:lib/JFLAP-7.0_With_Source.jar:lib/owl-18.10-snapshot.jar:lib/ejml/ejml-core-0.34.jar:lib/ejml/ejml-cdense-0.34.jar:lib/ejml/ejml-ddense-0.34.jar:lib/ejml/ejml-fdense-0.34.jar:lib/ejml/ejml-simple-0.34.jar:lib/ejml/ejml-zdense-0.34.jar:lib/ejml/ejml-dsparse-0.34.jar:lib/ejml/ejml-experimental-0.34.jar:lib/ltl2buchi.jar" \
+    "${agent_opts[@]}" \
+    main.SortSolutions \
+    "${args[@]}"
